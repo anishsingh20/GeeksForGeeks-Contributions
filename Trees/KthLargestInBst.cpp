09150bd91109634599b7a048705f2e96e64c8436
Anish Singh Walia <loveeanish@gmail.com>
2018-04-11 10:20:57 +0530

    added the algorithm

--- Trees/KthLargestInBst.cpp
+++ Trees/KthLargestInBst.cpp
@@ -1,4 +1,32 @@
 /*Finding kth largest node using Morris traversal in constant space in a BST.
+ALGORITHM-
+
+1) Initialize Current as root.
+
+2) Initialize a count variable to 0.
+
+3) While current is not NULL :
+
+  3.1) If current has no right child
+   a) Increment count and check if count is equal to K.
+      1)If count is equal to K, simply return current node as it is the Kth largest node.
+
+   b)Otherwise, Move to the left child of current.
+
+  3.2) Else, here we have 2 cases:
+   a) Find the inorder successor of current node. 
+      Inorder successor is the left most node 
+      in the right subtree or right child itself.
+   b) If the left child of the inorder successor is NULL:
+      1) Set current as the left child of its inorder successor.
+      2) Move current node to its right.
+   c) Else, if the threaded link between the current node 
+      and it's inorder successor already exists :
+      1) Set left pointer of the inorder successor as NULL.
+      2) Increment count and check if count is equal to K.
+           a)If count is equal to K, simply return current node as it is the Kth largest node.
+
+      3)Otherwise, Move current to it's left child.
 */
 
 
