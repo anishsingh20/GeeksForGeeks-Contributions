00125b2deac81da696efd285bf972561b54bccad
Anish Singh Walia <loveeanish@gmail.com>
2018-06-12 10:09:34 +0530

    some explanation about Morris traversal

--- Trees/closestInBSTusingMorrisTraversal.cpp
+++ Trees/closestInBSTusingMorrisTraversal.cpp
@@ -1,1 +1,4 @@
-
+/*
+Morris traversal is based on Threaded Binary trees which makes use of NULL pointers in a tree to make them point to some successor or predecessor nodes. 
+As in a binary tree with n nodes, n+1 NULL pointers waste memory.
+*/
